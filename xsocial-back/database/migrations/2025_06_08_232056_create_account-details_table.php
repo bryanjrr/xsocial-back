073fb9043871365efa560d7af9b49103c78dbf00
bryaanjrr <bryanjoyarubio@gmail.com>
2025-06-09 01:43:43 +0200

    Migrations of table users and account_details

---
+++ xsocial-back/database/migrations/2025_06_08_232056_create_account-details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('account_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_user');
+            $table->string('full_name');
+            $table->integer('phone');
+            $table->date('birthdate');
+            $table->string('location');
+            $table->longText('biography');
+            $table->foreign('id_user')->references('id_user')->on('user')->onDelete('cascade');;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+        Schema::dropIfExists('job_batches');
+        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('user');
+        Schema::dropIfExists('account_details');
+    }
+};
